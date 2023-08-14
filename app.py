2066eb71cbb25f3b3c961cd14a0fe44828f7e2d8
Student <darthkitten2228@gmail.com>
2023-08-14 12:18:40 +0530

    Added Routes for all pages

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,templating
+from sql import *
 
 
 app = Flask(__name__)
@@ -7,4 +8,37 @@ app = Flask(__name__)
 @app.route('/', methods = ['GET','POST'])
 def home():
    return templating.render_template("home.html")
-   
+
+# Account Page
+@app.route('/account',methods = ['GET','POST'])
+def account():
+   return templating.render_template("account.html")
+
+# Search Page
+@app.route('/search',methods = ['GET','POST'])
+def search():
+   return templating.render_template("search.html")
+
+# Product Page
+@app.route('/search/<product_name>',methods = ['GET','POST'])
+def product():
+   return templating.render_template("product.html")
+
+# Order Page
+@app.route('/order',methods = ['GET','POST'])
+def order():
+   return templating.render_template("order.html")
+
+# Status (For Delivery) Page
+@app.route('/order/status',methods = ['GET','POST'])
+def status():
+   return templating.render_template("status.html")
+
+# Sell Page
+@app.route('/sell',methods = ['GET','POST'])
+def sell():
+   return templating.render_template("sell.html")
+
+
+if __name__ =="__main__":  
+    app.run(debug = True)
