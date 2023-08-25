22fb63b05aed648b34ee5fb76243030c2f82c11a
Sathvik Mulukutla <darthkitten2228@gmail.com>
2023-08-25 11:24:53 +0530

    added redirect to signin

--- app.py
+++ app.py
@@ -1,61 +1,77 @@
-from flask import Flask,templating,session
+from flask import Flask, templating, session, redirect
 from sql import *
 
 
 app = Flask(__name__)
 
 # Home Page
-@app.route('/', methods = ['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
-   return templating.render_template("home.html")
+    return templating.render_template("home.html")
+
+
+# Sign in Page
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    return templating.render_template("signin.html")
+
 
 # Account Page
-@app.route('/account',methods = ['GET','POST'])
+@app.route('/account', methods=['GET', 'POST'])
 def account():
-   if 'username' in session:
-      return templating.render_template("account.html")
-   else:
-      return templating.render_template('signin.html')
+    while 'username' not in session:
+        return redirect('/signin')
+
+    return templating.render_template("account.html")
+
 
 # Search Page
-@app.route('/search',methods = ['GET','POST'])
+@app.route('/search', methods=['GET', 'POST'])
 def search():
-   return templating.render_template("search.html")
+    return templating.render_template("search.html")
+
 
 # Categories Page
-@app.route('/categories',methods = ['GET','POST'])
+@app.route('/categories', methods=['GET', 'POST'])
 def categories():
-   return templating.render_template("categories.html")
+    return templating.render_template("categories.html")
 
-#Category Page
-@app.route('/category/<category_name>',methods = ['GET','POST'])
+
+# Category Page
+@app.route('/category/<category_name>', methods=['GET', 'POST'])
 def category():
-   return templating.render_template("category.html")
+    return templating.render_template("category.html")
+
 
 # Product Page
-@app.route('/search/<product_name>',methods = ['GET','POST'])
+@app.route('/search/<product_name>', methods=['GET', 'POST'])
 def product():
-   return templating.render_template("product.html")
+    return templating.render_template("product.html")
+
 
 # Order Page
-@app.route('/order',methods = ['GET','POST'])
+@app.route('/order', methods=['GET', 'POST'])
 def order():
-   return templating.render_template("order.html")
+    return templating.render_template("order.html")
+
 
 # Status (For Delivery) Page
-@app.route('/status',methods = ['GET','POST'])
+@app.route('/status', methods=['GET', 'POST'])
 def status():
-   if 'username' in session:
-      return templating.render_template("status.html")
-   else:
-      return templating.render_template('signin.html')
-# Sell Page
-@app.route('/sell',methods = ['GET','POST'])
-def sell():
-   if 'username' in session:
-      return templating.render_template("account.html")
-   else:
-      return templating.render_template('sell.html')
+    while 'username' not in session:
+        return redirect('/signin')
 
-if __name__ =="__main__":  
-    app.run(debug = True)
+    return templating.render_template("status.html")
+
+
+# Sell Page
+@app.route('/sell', methods=['GET', 'POST'])
+def sell():
+    while 'username' not in session:
+        return redirect('/signin')
+
+    return templating.render_template("sell.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
