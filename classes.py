959bb8ac7c82389ae8e1d02888da63d1a6a87b3d
Student <darthkitten2228@gmail.com>
2023-08-29 13:36:37 +0530

    Started Product Class

--- classes.py
+++ classes.py
@@ -1,2 +1,18 @@
-class User():
-    pass
+from sql import *
+
+class Product:
+    
+    def __init__(self,name,description,price,discount,stock):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.discount = discount
+        self.discounted_price = self.price * self.discount
+    
+    
+    def has_stock(self,stock):
+        return lambda stock: True if stock > 0 else False
+    
+    
+    
+    
