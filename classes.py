a51ebd7890c42ee411bbbe887e20c6ff88f1bc37
Student <darthkitten2228@gmail.com>
2023-09-05 12:29:58 +0530

    Added to Product Class

--- classes.py
+++ classes.py
@@ -1,17 +1,30 @@
 from sql import *
+from statistics import mean
 
 class Product:
     
-    def __init__(self,name,description,price,discount,stock):
+    def __init__(self,category,name,description,price,discount,stock):
+        self.category = category
         self.name = name
         self.description = description
         self.price = price
         self.discount = discount
         self.discounted_price = self.price * self.discount
+        self.stock = stock
+        self.reviews = []
+        self.avgrating = mean(self.reviews)
+    
+    def has_stock(self):
+        return lambda stock: True if self.stock > 0 else False
+    
+    def insert_review(self,user,rating,review):
+        self.reviews.append({'user': user,
+                             'rating': rating,
+                             'review': review})
     
     
-    def has_stock(self,stock):
-        return lambda stock: True if stock > 0 else False
+    
+    
     
     
     
