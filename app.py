7b3c56ed2cc60024aee4a91c81ed644efa8c040a
DarthKitten <darthkitten2228@gmail.com>
2025-11-09 03:09:27 +0530

    Revert "fixed link issue"
    This reverts commit 337157be0306217705509740c4e4f9733f30f122.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, templating, redirect, request, session
 import pandas as pd
 from sql import *
-import os
 
 app = Flask(__name__)
 app.secret_key = 'root'
@@ -14,7 +13,7 @@ app.config['UPLOAD_FOLDER'] = './static/images'
 def home():
     create_table()
     products = fetch_category('home')
-    return render_template("home.html", products=products)
+    return templating.render_template("home.html",products = products)
 
 
 # Sign in Page
@@ -33,12 +32,14 @@ def signin():
 
             case 'verified':
                 session['username'] = request.form['username']
+                session['password'] = request.form['password']
                 return redirect('/account')
 
-    return render_template("signin.html", message=alert_message)
-
+    return templating.render_template("signin.html", message=alert_message)
 
 # Sign Out Route
+
+
 @app.route('/signout', methods=['GET', 'POST'])
 def signout():
     session.clear()
@@ -65,7 +66,7 @@ def createaccount():
             case 'success':
                 return redirect('/signin')
 
-    return render_template("createaccount.html", message=alert_message)
+    return templating.render_template("createaccount.html", message=alert_message)
 
 
 # Account Page
@@ -73,29 +74,23 @@ def createaccount():
 def account():
     if 'username' not in session:
         return redirect('/signin')
-
+    
     products = fetch_user(session['username'])
-    if products is None:
-        products = pd.DataFrame()
     address = fetch_address(session['username'])
     oh = order_history(session['username'])
-    if oh is None:
-        oh = pd.DataFrame()
 
-    buser = 'Yes' if len(products) > 0 else 'No'
+    if len(products) == 0:
+        buser = 'No'
 
-    if request.method == 'POST':
-        insert_cc(session['username'],
-                  request.form['card'], request.form['cvv'])
+    else:
+        buser = 'Yes'
 
-    return render_template(
-        "account.html",
-        username=session['username'],
-        products=products.to_html(classes='table table-striped', index=False),
-        address=address,
-        buser=buser,
-        oh=oh.to_html(classes='table table-striped', index=False)
-    )
+    if request.method=='POST':
+        insert_cc(session['username'],request.form['card'],request.form['cvv'])
+
+    return templating.render_template("account.html", username=session['username'], 
+                                      products = products.to_html(classes='table table-striped',index=False),address = address,
+                                      buser=buser,oh = oh.to_html(classes='table table-striped',index=False))
 
 
 # Search Page
@@ -103,28 +98,17 @@ def account():
 def search():
     if request.method == "POST":
         matches = engine(request.form['search'])
-        if matches is None:
-            matches = pd.DataFrame()
-        return render_template(
-            "search.html",
-            matches=matches.to_html(
-                classes='table table-striped', index=False, render_links=True)
-        )
-    return render_template("search.html")
+        return templating.render_template("search.html", matches=matches.to_html(classes='table table-striped',
+                                                                                     index=False, render_links=True))
+    return templating.render_template("search.html")
 
 
 # Category Page
 @app.route('/category/<category_name>', methods=['GET', 'POST'])
 def category(category_name):
     results = fetch_category(category=category_name)
-    if results is None:
-        results = pd.DataFrame()
-    return render_template(
-        "category.html",
-        category=category_name,
-        results=results.to_html(
-            classes='table table-striped', index=False, justify='center', render_links=True)
-    )
+    return templating.render_template("category.html", category=category_name, results=results.to_html(classes='table table-striped',
+                                                                                                       index=False, justify='center', render_links=True))
 
 
 # Product Page
@@ -133,16 +117,7 @@ def product(product_name):
     results = fetch_product(product_name)
     if request.method == 'POST':
         session['orderid'] = request.form['orderid']
-
-    # Fix for DataFrame access
-    category_name = results['category'].iloc[0] if not results.empty else 'home'
-
-    return render_template(
-        "product.html",
-        results=results,
-        product_name=product_name,
-        more_products=more_products(category_name)
-    )
+    return templating.render_template("product.html", results=results,product_name = product_name,more_products = more_products(results.category))
 
 
 # Order Page
@@ -150,26 +125,21 @@ def product(product_name):
 def order():
     if 'username' not in session:
         return redirect('/signin')
-
-    product = fetch_product(session.get('orderid'))
+    
+    
+    product = fetch_product(session['orderid'])
     address = fetch_address(session['username'])
     cc = fetch_cc(session['username'])
     alert_message = ""
 
     if request.method == 'POST':
         if request.form['cvv'] == fetch_cvv(request.form['card']):
-            update_stock(session['username'], session['orderid'])
+            update_stock(session['username'],session['orderid'])
         else:
-            alert_message = 'Sorry, that is the wrong CVV for this credit card'
-
-    return render_template(
-        "order.html",
-        orderid=session.get('orderid'),
-        product=product,
-        cc=cc,
-        address=address,
-        alert_message=alert_message
-    )
+            alert_message = 'Sorry, that is the wrong cvv for this credit card'
+        
+    return templating.render_template("order.html",orderid = session['orderid'], product = product, cc = cc, address = address,
+                                      alert_message=alert_message)
 
 
 # Sell Page
@@ -190,18 +160,13 @@ def sell():
         )
 
         f = request.files['image']
-        filename = f"{id}.jpg"
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        f.save('./static/images/'+str(id)+'.jpg')
+
 
     results = fetch_user(session['username'])
-    if results is None:
-        results = pd.DataFrame()
-
-    return render_template(
-        "sell.html",
-        results=results.to_html(classes='table table-striped', index=False)
-    )
+    return templating.render_template("sell.html", results=results.to_html(classes='table table-striped',
+                                                                           index=False))
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    app.run(host="0.0.0.0",port=10000)
